08cda830461db50f959af52f6526377b1f30405d
dtk77 <vasilenko.andrey77@outlook.com>
2022-02-04 20:17:15 +0700

    Add project files.

---
+++ Notes.ApplicationCore/Entities/Notes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.ApplicationCore
+{
+    internal class Notes : NotesBase
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public override DateTime CreatData { get; set; }
+        public override DataType? EditDate { get; set; }
+
+       
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Notes.ApplicationCore/Entities/NotesBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes.ApplicationCore
+{
+    internal abstract class NotesBase
+    {
+        public abstract DateTime CreatData { get; set; }
+        public abstract DataType? EditDate { get; set; }
+    }
+}
